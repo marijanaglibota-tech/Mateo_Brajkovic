d9104aabfc11a412d6e23c7cbb99a05f30b68df2
marijanaglibota-tech <marijana.glibota@skole.hr>
2026-04-16 15:59:54 +0200

    Mateo Brajković

---
+++ Mateo Brajkovic/script.js
@@ -0,0 +1,5 @@
+// JavaScript je trenutno prazan,
+// ali ovdje kasnije možeš dodati interakcije,
+// npr. alert, promjenu boja, animacije, klik događaje itd.
+
+console.log("Stranica Povijest Solina je učitana.");// JavaScript source code
